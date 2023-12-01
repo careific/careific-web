d6f4eb9101b03a0a1ea467d1de4dd983c9d57051
JuJu222 <64346619+JuJu222@users.noreply.github.com>
2023-12-01 09:53:12 +0700

    Added privacy policy

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,6 @@
+<section class="bg-white">
+    <div class='py-8 px-4 mx-auto max-w-screen-xl lg:py-12 lg:px-12'>
+        <h1 class="text-2xl font-bold mb-2">About</h1>
+
+    </div>
+</section>
--- src/routes/privacy-policy/+page.svelte
+++ src/routes/privacy-policy/+page.svelte
@@ -1,2 +1,101 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<section class="bg-white">
+    <div class='py-8 px-4 mx-auto max-w-screen-xl lg:py-12 lg:px-12'>
+        <h1 class="text-2xl font-bold mb-2">Privacy Policy</h1>
+        <p class="mb-4"><span class="font-semibold">Last Updated:</span> 30 November 2023</p>
+
+        <h2 class="text-xl font-semibold mb-2">1. Purpose and Scope</h2>
+        <h3 class="pl-5 text-lg font-semibold mb-2">a. Purpose</h3>
+        <p class="pl-5 mb-2">Careific is dedicated to providing caregivers with a comprehensive tool to enhance communication and monitoring of their seniors, fostering a secure and connected caregiving experience.</p>
+        <p class="pl-5 mb-3">The collection and utilization of personal and health data are strictly for the purpose of improving the caregiving process, ensuring safety, and providing valuable insights for both caregivers and seniors.</p>
+        <h3 class="pl-5 text-lg font-semibold mb-2">b. Scope</h3>
+        <p class="pl-5 mb-2">This Privacy Policy applies to all users of Careific, encompassing caregivers, seniors, and any third-party service providers involved in the functionality of the app.</p>
+        <p class="pl-5 mb-4">The policy governs the collection, use, and sharing of personal and health data within the app.</p>
+
+        <h2 class="text-xl font-semibold mb-2">2. Information We Collect</h2>
+        <h3 class="pl-5 text-lg font-semibold mb-2">a. Personal Information:</h3>
+        <h5 class="pl-5 font-semibold mb-2">Caregiver</h5>
+        <ul class="list-disc pl-9 mb-3">
+            <li>We collect your name, email address, and password for authentication purposes.</li>
+            <li>The location that is set as a geofence for your senior.</li>
+            <li>Your voice from your device’s microphone if the walkie-talkie feature is used.</li>
+            <li>Device tokens, which are unique identifiers associated with your device, are used for authentication, push notifications, and communication features.</li>
+            <li>The seniors whom you are linked to.</li>
+        </ul>
+        <h5 class="pl-5 font-semibold mb-2">Senior</h5>
+        <ul class="list-disc pl-9 mb-3">
+            <li>We collect your name, email address, and password for authentication purposes.</li>
+            <li>Data that is automatically collected for health monitoring, including fall detection, heart rate, heart anomaly, device motion, location, and battery as well as charging status.</li>
+            <li>Data provided by the user including reported symptoms, SOS button presses, scheduled routines, and medicines.</li>
+            <li>Your voice from your device’s microphone if the walkie-talkie feature is used.	</li>
+            <li>Device tokens, which are unique identifiers associated with your device, are used for authentication, push notifications, and communication features.</li>
+            <li>The caregivers whom you are linked to.</li>
+        </ul>
+        <h3 class="pl-5 text-lg font-semibold mb-2">b. Usage Information</h3>
+        <p class="mb-4 pl-5">Information on how you interact with the app, including active sessions.</p>
+
+        <h2 class="text-xl font-semibold mb-2">3. How We Use Your Information</h2>
+        <h3 class="pl-5 text-lg font-semibold mb-2">a. Authentication</h3>
+        <ul class="list-disc pl-9 mb-3">
+            <li>Securely authenticate users through Firebase Authentication, ensuring a protected and personalized user experience.</li>
+        </ul>
+        <h3 class="pl-5 text-lg font-semibold mb-2">b. Communication</h3>
+        <ul class="list-disc pl-9 mb-3">
+            <li>Facilitate seamless communication between caregivers and seniors.</li>
+            <li>Send notifications related to fall detection, location geofencing, inactivity monitoring, charging status, and more to caregivers regarding the linked seniors condition.</li>
+        </ul>
+        <h3 class="pl-5 text-lg font-semibold mb-2">c. Health and Safety Monitoring</h3>
+        <ul class="list-disc pl-9 mb-3">
+            <li>Monitor health metrics such as heart rate, heart anomaly, and provide insights from the senior’s Apple Watch for caregivers.</li>
+            <li>Allow caregivers to monitor the battery status, charging status, and device motion status of the senior's iPhone and Apple Watch.</li>
+            <li>Enable health consultations through the GPT feature, powered by OpenAI's GPT API for caregivers.</li>
+            <li>Enable location geofencing for tracking and enhancing safety measures for seniors.</li>
+        </ul>
+        <h3 class="pl-5 text-lg font-semibold mb-2">d. Safety and Emergency Response</h3>
+        <ul class="list-disc pl-9 mb-3">
+            <li>Utilize fall detection and the SOS button for seniors to provide prompt alerts for caregivers.</li>
+        </ul>
+        <h3 class="pl-5 text-lg font-semibold mb-2">e. Reminders and Tasks</h3>
+        <ul class="list-disc pl-9 mb-3">
+            <li>Provide timely reminders for seniors regarding medicine intake, daily routine, and other caregiving tasks.</li>
+        </ul>
+        <h3 class="pl-5 text-lg font-semibold mb-2">e. Symptom Reporting</h3>
+        <ul class="list-disc pl-9 mb-3">
+            <li>Allow seniors to report their symptoms to caregivers, promoting open communication and proactive caregiving.</li>
+        </ul>
+        <h3 class="pl-5 text-lg font-semibold mb-2">f. Data History</h3>
+        <ul class="list-disc pl-9 mb-4">
+            <li>Maintain a comprehensive history of all information collected above for both caregivers and seniors.</li>
+        </ul>
+
+        <h2 class="text-xl font-semibold mb-2">4. Sharing of Information</h2>
+        <h3 class="pl-5 text-lg font-semibold mb-2">a. Between Caregiver and Senior</h3>
+        <p class="pl-5 mb-3">Information is shared securely between caregivers and seniors through the app for effective communication and monitoring.</p>
+        <h3 class="pl-5 text-lg font-semibold mb-2">b. Service Providers</h3>
+        <p class="pl-5 mb-3">We may share data with trusted third-party service providers for app functionality, analytics, and customer support.</p>
+        <h3 class="pl-5 text-lg font-semibold mb-2">c. Legal Compliance</h3>
+        <p class="pl-5 mb-4">We may disclose information as required by law or to protect our rights and users.</p>
+
+        <h2 class="text-xl font-semibold mb-2">5. Data Security</h2>
+        <p class="pl-5 mb-4">Industry-standard security measures are implemented to protect your data from unauthorized access or disclosure.</p>
+
+        <h2 class="text-xl font-semibold mb-2">6. Authentication and Push Notifications</h2>
+        <p class="pl-5 mb-2">Careific uses Firebase Authentication for secure user authentication.</p>
+        <p class="pl-5 mb-4">Firebase Cloud Messaging is employed for reliable push notifications.</p>
+
+        <h2 class="text-xl font-semibold mb-2">7. Consultation Feature</h2>
+        <p class="pl-5 mb-2">Careific leverages OpenAI's GPT API to power the health consultation feature, ensuring accurate and insightful information.</p>
+        <p class="pl-5 mb-4">Queries from the user will be sent to OpenAI's GPT API, which is under OpenAI’s privacy policy. We recommend you to review their privacy policy before using this feature.</p>
+
+        <h2 class="text-xl font-semibold mb-2">8. Updates to Privacy Policy</h2>
+        <p class="pl-5 mb-4">We may update this Privacy Policy periodically to reflect changes in our practices. Users are encouraged to check for updates regularly.</p>
+
+        <h2 class="text-xl font-semibold mb-2">9. Contact Us</h2>
+        <p class="pl-5 mb-4">If you have any questions or concerns about our Privacy Policy, please reach out to us at yteamapplication@gmail.com.</p>
+
+        <p class="mb-4">By using Careific, you acknowledge and agree to the terms outlined in this Privacy Policy. We recommend reading it thoroughly to ensure a clear understanding of our practices concerning your information. Your privacy and trust are paramount to us, and we are committed to providing a secure and transparent caregiving experience through our app.</p>
+
+        <!--        <p class="mb-4">Jika Anda memiliki pertanyaan atau masalah mengenai syarat dan ketentuan ini,-->
+        <!--            silakan hubungi kami di alamat email berikut: <a class='underline'-->
+        <!--                                                             href="mailto:info@contohwebsite.com">info@indoipo.com</a></p>-->
+    </div>
+</section>
